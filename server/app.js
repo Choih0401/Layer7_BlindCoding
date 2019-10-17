e066a5b9006217ff03d296c4e26fa4703959be1a
Choih0401 <danielch0401@naver.com>
2019-10-17 22:12:05 +0900

    Make server and client directory
    Replace db_con.js
    Replace db_conf.js
    Replace v1.js
    Replace api.js
    Replace app.js
    Replace index.js

--- server/app.js
+++ server/app.js
@@ -11,20 +11,20 @@ var app = express()
 
 app.set('port', process.env.PORT || 5000)
 
-http.createServer(app).listen(app.get('port'), function () {
-  console.log('Express server listening on port ' + app.get('port'))
+http.createServer(app).listen(app.get('port'), function() {
+    console.log('Express server listening on port ' + app.get('port'))
 })
 
 app.use(logger('dev'))
 app.use(express.json())
 app.use(express.urlencoded({
-  extended: false
+    extended: false
 }))
 app.use(cookieParser())
 app.use(express.static(path.join(__dirname, 'public')))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({
-  extended: false
+    extended: false
 }))
 
 app.use(cors())
@@ -32,21 +32,21 @@ app.use(cors())
 const v1 = require('./routes/v1/v1')
 app.use('/v1', v1)
 
-app.use(function (req, res, next) {
-  next(res.json({
-    code: 404,
-    v: 'v1',
-    status: 'ERR_NOTFOUND'
-  }))
+app.use(function(req, res, next) {
+    next(res.json({
+        code: 404,
+        v: 'v1',
+        status: 'ERR_NOTFOUND'
+    }))
 })
 
-app.use(function (err, req, res, next) {
-  res.locals.message = err.message
-  res.locals.error = req.app.get('env') === 'development' ? err : {}
-  res.status(err.status || 500)
-  res.json({
-    code: 500,
-    v: 'v1',
-    status: 'ERR_SERVER'
-  })
+app.use(function(err, req, res, next) {
+    res.locals.message = err.message
+    res.locals.error = req.app.get('env') === 'development' ? err : {}
+    res.status(err.status || 500)
+    res.json({
+        code: 500,
+        v: 'v1',
+        status: 'ERR_SERVER'
+    })
 })
--- server/routes/v1/v1.js
+++ server/routes/v1/v1.js
@@ -4,7 +4,11 @@ import * as API from './api'
 const router = express.Router()
 
 router.get('/', (req, res) => {
-  res.json({ code: 200, v: 'v1', status: 'OK' })
+    res.json({
+        code: 200,
+        v: 'v1',
+        status: 'OK'
+    })
 })
 
 router.post('/auth/signup', API.signUp) // 회원가입
