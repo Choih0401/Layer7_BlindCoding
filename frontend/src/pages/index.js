d867adc3a3eeeb184b85a8d07cfffb43bcc74e94
YeonGyu <code.yeon.gyu@gmail.com>
2019-10-31 00:58:54 +0900

    Add Problem Page

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -1,4 +1,4 @@
 export {default as Login} from './Login';
 export {default as Rank} from './Rank';
-export {default as Code} from './Code';
+export {default as Problem} from './Problem';
 export {default as Home} from './Home';
