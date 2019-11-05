b13d4ae80cc8c02be756fb33819a4d59bdfd6d34
Choih0401 <danielch0401@naver.com>
2019-11-05 21:45:20 +0900

    fix

--- server/routes/v1/api.js
+++ server/routes/v1/api.js
@@ -6,7 +6,7 @@ var spawn = require('child_process').spawn;
 import async from 'async'
 require('dotenv').config()
 
-export const signUp = function (req, res) {
+export const signUp = function(req, res) {
     var {
         id,
         name,
@@ -25,7 +25,7 @@ export const signUp = function (req, res) {
         async.waterfall([
                 (callback) => {
                     password = crypto.createHash('sha512').update(crypto.createHash('sha512').update(password).digest('base64')).digest('base64');
-                    var sql = 'SELECT * FROM user_list WHERE id = ? AND is_use = 1'
+                    var sql = 'SELECT count(*) as count FROM user_list WHERE id = ?'
                     connection.query(sql, [id], (err, result) => {
                         if (err) {
                             callback({
@@ -33,24 +33,27 @@ export const signUp = function (req, res) {
                                 message: 'QUERY ERROR'
                             })
                         } else {
-                            if (result.length > 0) {
+                            if (result[0].count > 0) {
                                 callback({
                                     err: 'ERR_SIGNUP',
                                     message: 'USERID ALREADY EXISTS'
                                 })
                             } else {
-                                var sql2 = 'INSERT INTO user_list (id, name, password, email, phonenumber) values(?, ?, ?, ?, ?)'
-                                connection.query(sql2, [id, name, password, email, phonenumber], (err, result) => {
-                                    if (err) {
-                                        callback({
-                                            err: 'QUERY',
-                                            message: 'QUERY ERROR02'
-                                        })
-                                    } else {
-                                        callback(null, '')
-                                    }
-                                })
+                                callback(null, '')
                             }
+                        }
+                    })
+                },
+                (resultData, callback) => {
+                    var sql = 'INSERT INTO user_list (id, name, password, email, phonenumber) values(?, ?, ?, ?, ?)'
+                    connection.query(sql, [id, name, password, email, phonenumber], (err, result) => {
+                        if (err) {
+                            callback({
+                                err: 'QUERY',
+                                message: 'QUERY ERROR'
+                            })
+                        } else {
+                            callback(null, '')
                         }
                     })
                 }
@@ -75,7 +78,7 @@ export const signUp = function (req, res) {
     }
 }
 
-export const signIn = function (req, res) {
+export const signIn = function(req, res) {
     var {
         id,
         password
@@ -131,7 +134,7 @@ export const signIn = function (req, res) {
     }
 }
 
-export const compile = function (req, res) {
+export const compile = function(req, res) {
     var {
         language,
         content
@@ -146,71 +149,59 @@ export const compile = function (req, res) {
     } else {
         async.waterfall([
                 (callback) => {
-                    if (language == 'c') {
+                    if(language == 'c'){
                         let file = 'code/code.c'
-                        fs.writeFile(file, content, 'utf8', function (err) {})
+                        fs.writeFile(file, content, 'utf8', function(err){
+                        })
                         var compile = spawn('gcc', [file])
-                        compile.on('close', function (data) {
-                            if (data == 0) {
+                        compile.on('close', function(data){
+                            if(data == 0){
                                 var run = spawn('./a.out', [])
-                                run.stdout.on('data', function (output) {
-                                    callback(null, {
-                                        output: output.toString('utf8')
-                                    })
+                                run.stdout.on('data', function(output){
+                                    callback(null, {output: output.toString('utf8')})
                                 })
-                            } else {
-                                callback({
-                                    err: '.c code'
-                                })
+                            }else{
+                                callback({err: '.c code'})
                             }
                         })
-                    } else if (language == 'cpp') {
+                    }else if(language == 'cpp'){
                         let file = 'code/code.cpp'
-                        fs.writeFile(file, content, 'utf8', function (err) {})
+                        fs.writeFile(file, content, 'utf8', function(err){
+                        })
                         var compile = spawn('g++', [file])
-                        compile.on('close', function (data) {
-                            if (data == 0) {
+                        compile.on('close', function(data){
+                            if(data == 0){
                                 var run = spawn('./a.out', [])
-                                run.stdout.on('data', function (output) {
-                                    callback(null, {
-                                        output: output.toString('utf8')
-                                    })
+                                run.stdout.on('data', function(output){
+                                    callback(null, {output: output.toString('utf8')})
                                 })
-                            } else {
-                                callback({
-                                    err: '.cpp code'
-                                })
+                            }else{
+                                callback({err: '.cpp code'})
                             }
                         })
-                    } else if (language == 'python2') {
+                    }else if(language == 'python2'){
                         let file = 'code/code.py'
-                        fs.writeFile(file, content, 'utf8', function (err) {})
+                        fs.writeFile(file, content, 'utf8', function(err){
+                        })
                         var compile = spawn('python', [file])
-                        compile.stdout.on('data', function (data) {
-                            callback(null, {
-                                output: data.toString('utf8').replace(/\n+$/, '')
-                            })
+                        compile.stdout.on('data', function(data){
+                            callback(null, {output: data.toString('utf8').replace(/\n+$/,'')})
                         })
-                        compile.stderr.on('data', function (data) {
-                            callback({
-                                err: String(data)
-                            })
+                        compile.stderr.on('data', function(data){
+                            callback({err: String(data)})
                         })
-                    } else if (language == 'python3') {
+                    }else if(language == 'python3'){
                         let file = 'code/code.py'
-                        fs.writeFile(file, content, 'utf8', function (err) {})
+                        fs.writeFile(file, content, 'utf8', function(err){
+                        })
                         var compile = spawn('python3', [file])
-                        compile.stdout.on('data', function (data) {
-                            callback(null, {
-                                output: data.toString('utf8').replace(/\n+$/, '')
-                            })
+                        compile.stdout.on('data', function(data){
+                            callback(null, {output: data.toString('utf8').replace(/\n+$/,'')})
                         })
-                        compile.stderr.on('data', function (data) {
-                            callback({
-                                err: String(data)
-                            })
+                        compile.stderr.on('data', function(data){
+                            callback({err: String(data)})
                         })
-                    } else {
+                    }else{
                         res.json({
                             code: 500,
                             v: 'v1',
@@ -240,54 +231,51 @@ export const compile = function (req, res) {
     }
 }
 
-export const leaderboard = function (req, res) {
+export const leaderboard = function(req, res) {
     async.waterfall([
-            (callback) => {
-                var sql = 'SELECT * FROM user_list WHERE is_use = 0 ORDER BY question desc, score, endtime'
-                connection.query(sql, [], (err, result) => {
-                    if (err) {
-                        callback({
-                            err: 'QUERY',
-                            message: 'QUERY ERROR'
-                        })
-                    } else {
-                        let arr = []
-                        if (result.length != 0) {
-                            result.forEach((v, i) => {
-                                arr.push({
-                                    name: v.name,
-                                    score: v.score,
-                                    question: v.question
-                                })
+        (callback) => {
+            var sql = 'SELECT * FROM user_list WHERE is_use = 0 ORDER BY question desc, score, endtime'
+            connection.query(sql, [], (err, result) => {
+                if (err) {
+                    callback({err: 'QUERY', message: 'QUERY ERROR'})
+                }else{
+                    let arr = []
+                    if(result.length != 0){
+                        result.forEach((v, i) => {
+                            arr.push({
+                                name: v.name,
+                                score: v.score,
+                                question: v.question
                             })
-                            callback(null, arr)
-                        } else {
-                            callback(null, arr)
-                        }
+                        })
+                        callback(null, arr)
+                    }else{
+                        callback(null, arr)
                     }
-                })
-            }
-        ],
-        (err, result) => {
-            if (err) {
-                res.json({
-                    code: 500,
-                    v: 'v1',
-                    status: 'ERR_LEADERBOARD',
-                    detail: err
-                })
-            } else {
-                res.json({
-                    code: 200,
-                    v: 'v1',
-                    status: 'SUCCESS',
-                    detail: result
-                })
-            }
-        })
+                }
+            })
+        }
+    ],
+    (err, result) => {
+        if (err) {
+            res.json({
+                code: 500,
+                v: 'v1',
+                status: 'ERR_LEADERBOARD',
+                detail: err
+            })
+        } else {
+            res.json({
+                code: 200,
+                v: 'v1',
+                status: 'SUCCESS',
+                detail: result
+            })
+        }
+    })
 }
 
-export const updateScore = function (req, res) {
+export const updateScore = function(req, res) {
     var {
         id
     } = req.body
@@ -356,58 +344,58 @@ export const updateScore = function (req, res) {
     }
 }
 
-export const timeCompare = function (req, res) {
+export const timeCompare = function(req, res) {
     var {
         id
     } = req.body
     var old, now, gap, sec_gap
     async.waterfall([
-            (callback) => {
-                var sql = 'SELECT starttime, endtime FROM user_list WHERE id = ?'
-                connection.query(sql, [id], (err, result) => {
-                    if (err) {
-                        callback({
-                            err: 'QUERY',
-                            message: 'QUERY ERROR'
-                        })
-                    } else {
-                        old = result[0].starttime
-                        now = result[0].endtime
-                        gap = now.getTime() - old.getTime()
-                        sec_gap = gap / 1000
-                        callback(null, '')
-                    }
-                })
-            },
-            (resultData, callback) => {
-                var sql = 'UPDATE user_list SET score = ?, is_use = 0 WHERE id = ?'
-                connection.query(sql, [sec_gap, id], (err, result) => {
-                    if (err) {
-                        callback({
-                            err: 'QUERY',
-                            message: 'QUERY ERROR'
-                        })
-                    } else {
-                        callback(null, '')
-                    }
-                })
-            }
-        ],
-        (err, result) => {
-            if (err) {
-                res.json({
-                    code: 500,
-                    v: 'v1',
-                    status: 'ERR_TIMECOMPARE',
-                    detail: err
-                })
-            } else {
-                res.json({
-                    code: 200,
-                    v: 'v1',
-                    status: 'SUCCESS',
-                    detail: 'Compare time successful'
-                })
-            }
-        })
+        (callback) => {
+            var sql = 'SELECT starttime, endtime FROM user_list WHERE id = ?'
+            connection.query(sql, [id], (err, result) => {
+                if(err){
+                    callback({
+                        err: 'QUERY',
+                        message: 'QUERY ERROR'
+                    })
+                }else{
+                    old = result[0].starttime
+                    now = result[0].endtime
+                    gap = now.getTime() - old.getTime()
+                    sec_gap = gap / 1000
+                    callback(null, '')
+                }
+            })
+        },
+        (resultData, callback) => {
+            var sql = 'UPDATE user_list SET score = ?, is_use = 0 WHERE id = ?'
+            connection.query(sql, [sec_gap, id], (err, result) => {
+                if(err){
+                    callback({
+                        err: 'QUERY',
+                        message: 'QUERY ERROR'
+                    })
+                }else{
+                    callback(null, '')
+                }
+            })
+        }
+    ],
+    (err, result) => {
+        if(err){
+            res.json({
+                code: 500,
+                v: 'v1',
+                status: 'ERR_TIMECOMPARE',
+                detail: err
+            })
+        }else{
+            res.json({
+                code: 200,
+                v: 'v1',
+                status: 'SUCCESS',
+                detail: 'Compare time successful'
+            })
+        }
+    })
 }
